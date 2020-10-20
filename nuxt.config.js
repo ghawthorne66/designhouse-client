ec44fa3a59b00a83cea9f8904e7a2f631271d61f
Greg Hawthorne <ghawthorne@me.com>
2020-10-20 08:28:15 +0200

    98 registration spinner

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -15,8 +17,16 @@ export default {
       // {src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js'},
       {src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js'},
 
-      { src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js", integrity: "sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN", crossorigin: "anonymous"},
-      { src:"https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js", integrity: "sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d", crossorigin: "anonymous"}
+      {
+        src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
+        integrity: "sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN",
+        crossorigin: "anonymous"
+      },
+      {
+        src: "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js",
+        integrity: "sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d",
+        crossorigin: "anonymous"
+      }
     ]
   },
 
@@ -37,7 +47,12 @@ export default {
   buildModules: [`@nuxtjs/router`],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: ['@nuxtjs/axios', '@nuxtjs/auth', 'bootstrap-vue/nuxt'],
+  modules: [
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/auth',
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
+  ],
 
   //Auth Token Based Configuration
 
@@ -45,9 +60,9 @@ export default {
     strategies: {
       local: {
         endpoints: {
-          login: { url: '/login', method: 'post', propertyName: 'token' },
-          logout: { url: '/logout', method: 'post' },
-          user: { url: '/me', method: 'get', propertyName: 'data' }
+          login: {url: '/login', method: 'post', propertyName: 'token'},
+          logout: {url: '/logout', method: 'post'},
+          user: {url: '/me', method: 'get', propertyName: 'data'}
         },
         // tokenRequired: true,
         // tokenType: 'bearer',
@@ -61,6 +76,5 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {}
 }
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,70 +1,84 @@
 <template>
-    <!-- Section Cards -->
-    <section class="authentication">
-      <div class="auth-body">
-        <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
-          Register
-        </h1>
-        <form class="auth-form">
-          <div class="form-group">
-            <input
-              type="text"
-              v-model.trim="form.name"
-              name="name"
-              class="form-control form-control-lg font-14 fw-300"
-              placeholder="Full Name"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="text"
-              v-model.trim="form.username"
-              name="username"
-              class="form-control form-control-lg font-14 fw-300"
-              placeholder="Username"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="text"
-              v-model.trim="form.email"
-              name="email"
-              class="form-control form-control-lg font-14 fw-300"
-              placeholder="Email"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              v-model.trim="form.password"
-              type="password"
-              name="password"
-              class="form-control form-control-lg font-14 fw-300"
-              placeholder="Password"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              v-model.trim="form.password_confirmation"
-              type="password"
-              name="password_confirmation"
-              class="form-control form-control-lg font-14 fw-300"
-              placeholder="Confirm Password"
-            />
-          </div>
+  <!-- Section Cards -->
+  <section class="authentication">
+    <div class="auth-body">
+      <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
+        Register
+      </h1>
+      <form class="auth-form" @submit.prevent="submit">
+        <div class="form-group">
+          <input
+            type="text"
+            v-model.trim="form.name"
+            name="name"
+            class="form-control form-control-lg font-14 fw-300"
+            :class="{ 'is-invalid': form.errors.has('name') }"
+            placeholder="Full Name"
+          />
+          <has-error :form="form" field="name"></has-error>
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            v-model.trim="form.username"
+            name="username"
+            class="form-control form-control-lg font-14 fw-300"
+            :class="{ 'is-invalid': form.errors.has('username') }"
+            placeholder="Username"
+          />
+          <has-error :form="form" field="username"></has-error>
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            v-model.trim="form.email"
+            name="email"
+            class="form-control form-control-lg font-14 fw-300"
+            :class="{ 'is-invalid': form.errors.has('email') }"
+            placeholder="Email"
+          />
+          <has-error :form="form" field="email"></has-error>
+        </div>
+        <div class="form-group">
+          <input
+            v-model.trim="form.password"
+            type="password"
+            name="password"
+            class="form-control form-control-lg font-14 fw-300"
+            :class="{ 'is-invalid': form.errors.has('password') }"
+            placeholder="Password"
+          />
+          <has-error :form="form" field="password"></has-error>
+        </div>
+        <div class="form-group">
+          <input
+            v-model.trim="form.password_confirmation"
+            type="password"
+            name="password_confirmation"
+            class="form-control form-control-lg font-14 fw-300"
+            placeholder="Confirm Password"
+          />
+        </div>
 
-          <div class="text-right">
-            <button type="submit" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
-              Register
-            </button>
-          </div>
-          <p class="font-14 fw-400 text-center mt-4">
-            Already have an account?
-            <a class="color-blue" href="#"> Login</a>
-          </p>
-        </form>
-      </div>
-    </section>
-    <!-- End Cards -->
+        <div class="text-right">
+          <button type="submit"
+                  :disabled="form.busy"
+                  class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
+              <span v-if="form.busy">
+              <i class="fas fa-spinner fa-spin"></i>
+
+              </span>
+            Register
+          </button>
+        </div>
+        <p class="font-14 fw-400 text-center mt-4">
+          Already have an account?
+          <a class="color-blue" href="#"> Login</a>
+        </p>
+      </form>
+    </div>
+  </section>
+  <!-- End Cards -->
 
 </template>
 
@@ -72,21 +86,31 @@
 export default {
   data() {
     return {
-  form: this.$vform({
-    username: '',
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation: ''
-  })
+      form: this.$vform({
+        username: '',
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      })
     }
   },
   methods: {
     submit() {
-
+      console.log('TEST')
+      this.form
+        .post('/register')
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 }
+
+
 </script>
 
 <style scoped>
