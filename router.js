7bb22fbcfb156a03f7e05b7e238227f13936df89
Greg Hawthorne <ghawthorne@me.com>
2020-10-20 08:45:18 +0200

    99 registration

--- router.js
+++ router.js
@@ -8,7 +8,9 @@ const page = path => () => import(`~/pages/${path}`).then( m => m.default || m);
 const routes = [
   {path: '/', name: 'index', component: page('index.vue')},
   {path: '/login', name: 'login', component: page('auth/login.vue')},
-  {path: '/register', name: 'register', component: page('auth/register.vue')}
+  {path: '/register', name: 'register', component: page('auth/register.vue')},
+  {path: '/verification/verify/:id', name: 'verify', component: page('auth/verification/verify.vue')}
+
   // {path: '/password-reset', name: 'password-reset', component: page('password/password-reset.vue')},
   // {path: '/reset-email', name: 'reset-email', component: page('password/reset-email.vue')}
 
