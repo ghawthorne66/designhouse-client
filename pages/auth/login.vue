bbe77c7703101a56548364595aa1ef1e486bf789
Greg Hawthorne <ghawthorne@me.com>
2020-10-20 09:13:56 +0200

    100 login w local strategy

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -4,46 +4,86 @@
       <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
         Login
       </h1>
-      <form class="auth-form" action="" method="">
+      <form class="auth-form" @submit.prevent="submit">
+        <alert-error v-if="form.errors.has('message')" :form="form">
+          {{ form.errors.get('message') }}
+          <nuxt-link :to="{ name: 'verification.resend' }"
+          >Resend verification email</nuxt-link
+          >
+        </alert-error>
         <div class="form-group">
           <input
             type="text"
-            name="email"
-            class="form-control form-control-lg font-14 fw-300"
+            :form="form"
+            field="email"
+            v-model="form.email"
             placeholder="Email"
-          />
+          >
         </div>
         <div class="form-group">
           <input
-            type="password"
-            name="password"
-            class="form-control form-control-lg font-14 fw-300"
+            type="text"
+            :form="form"
+            field="password"
+            inputType="password"
+            v-model="form.password"
             placeholder="Password"
-          />
+          >
         </div>
         <div class="mt-4 mb-4 clearfix">
-          <a class="forgot-pass color-blue font-14 fw-400" href="#"> Forgot password? </a>
+          <nuxt-link
+            to="/password/email"
+            class="forgot-pass color-blue font-14 fw-400"
+          >
+            Forgot password?
+          </nuxt-link>
         </div>
         <div class="text-right">
-          <button type="submit" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
+          <button :loading="form.busy">
             Login
           </button>
         </div>
         <p class="font-14 fw-400 text-center mt-4">
           Don't have an account yet?
-          <a class="color-blue" href="#"> Create an account</a>
+          <nuxt-link :to="{ name: 'register' }" class="color-blue">
+            Create an account
+          </nuxt-link>
         </p>
       </form>
     </div>
   </section>
 </template>
 
+
 <script>
 export default {
-  name: "login"
-}
+
+    // middleware: ['guest'],
+    data() {
+      return {
+        form: this.$vform({
+          email: '',
+          password: ''
+        })
+      }
+    },
+      methods: {
+        submit(){
+          this.$auth
+            .loginWith('local', {
+              data: this.form
+            })
+            .then(res => {
+              console.log(res);
+            })
+            .catch(e => {
+              this.form.errors.set(e.response.data.errors);
+            });
+        }
+      }
+    };
 </script>
 
-<style scoped>
+<style>
 
 </style>
