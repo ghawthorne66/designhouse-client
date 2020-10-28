3e6cef7dc6a7bf2bb787c6f0ae08d2a11c010797
Ramanjaneya Karnati <rkarnati@nisum.com>
2020-10-28 15:17:50 +0530

    design issue image fix

--- components/slim/slim.vue
+++ components/slim/slim.vue
@@ -1,33 +1,31 @@
-
 <template>
     <div class="slim">
+    <client-only>
         <slot></slot>
+    </client-only>
     </div>
 </template>
 
 <script>
-  // Slim (place slim CSS and slim.module.js file in same folder as this file)
-  import Slim from './slim.module.js'
-
-  var instance = null
-
+import Slim from './slim.module.js';
+var instance = null;
   export default {
     props: ['options'],
     name: 'slim-cropper',
     mounted: function () {
       if (this.options.initialImage) {
-        var img = document.createElement('img')
-        img.setAttribute('alt', '')
-        img.src = this.options.initialImage
-        this.$el.appendChild(img)
+      var img = document.createElement('img');
+      img.setAttribute('alt', '');
+      img.src = this.options.initialImage;
+      this.$el.appendChild(img);
       }
-      instance = new Slim(this.$el, this.options)
+    instance = new Slim(this.$el, this.options);
     },
     beforeDestroy: function () {
-      instance.destroy()
+    instance.destroy();
     }
-  }
+};
 </script>
 <style lang="css">
-    @import "./slim.min.css";
+@import './slim.min.css';
 </style>
